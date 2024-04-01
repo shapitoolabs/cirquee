ec4495554b13e83cb61ba5adae71ff7ec004e6b8
b4rtt <b4rt11@gmail.com>
2024-04-01 15:50:09 +0200

    clean

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-
-import axios from 'axios';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { API_URL } from '@/api/API';
 import { Button } from '@/components/Button';
@@ -10,35 +8,21 @@ import { useTranslations } from '@/hooks/useTranslation';
 import { TScreenProps } from '@/utils/navigation';
 
 export const HomeScreen: React.FC<TScreenProps> = ({ route, navigation }) => {
-  const [thez, setThez] = React.useState<string>('');
-  const [jezdeckasocha, setJezdeckasocha] = React.useState<string>('');
-
-  const loadDataThez = () => {
-    axios.get('https://ip.thez.info/?json').then((res) => {
-      alert('.thez ' + JSON.stringify(res.data));
-      setThez(JSON.stringify(res.data));
-    });
-  };
-
-  const loadDataJezdeckasocha = () => {
-    axios.get('https://jezdeckasocha.cz/?json').then((res) => {
-      alert('.jezdeckasocha ' + JSON.stringify(res.data));
-      setJezdeckasocha(JSON.stringify(res.data));
-    });
-  };
+  const { t } = useTranslations();
 
   return (
-    <View style={{ flex: 1 }}>
-      <SafeAreaView
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-      >
-        <Text>Ahoj</Text>
-        <Text>{thez}</Text>
-        <Text>{jezdeckasocha}</Text>
-
-        <Button text="Load .thez" onPress={loadDataThez} />
-        <Button text="Load .jezdeckasocha" onPress={loadDataJezdeckasocha} />
-      </SafeAreaView>
+    <View style={styles.container}>
+      <Button
+        onPress={() => {
+          navigation.navigate('SecondScreen');
+        }}
+        text="Second screen"
+      />
+      <Text style={{ fontFamily: 'Inter-Thin' }}>
+        APP NAME: {env.EXPO_PUBLIC_EAS_PROJECT_ID}
+        {t('HomeScreen.title')}
+        {API_URL}
+      </Text>
     </View>
   );
 };
